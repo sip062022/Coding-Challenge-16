a2602dc62bb10394d301e0034d516c2612ae0163
Sahara Sipple <sahara36@usf.edu>
2025-03-28 21:42:48 -0400

    Edits to task 2

--- product-dashboard-vanilla-js/cc_16.js
+++ product-dashboard-vanilla-js/cc_16.js
@@ -1,20 +1,17 @@
 // Task 2: Fetch Products With //
 
-const apiUrl = 'https://www.course-api.com/javascript-store-products'; // api URL
-const proxyUrl = 'https://cors-anywhere.herokuapp.com/'; // proxy URL constant to bypass CORS
-
 function fetchProductsThen() {  // function to fetch the products
-    fetch(proxyUrl + apiUrl) // fetches the urls for both apiUrl and proxyUrl
+    fetch('https://www.course-api.com/javascript-store-products') // fetches the urls for both apiUrl and proxyUrl
         .then(response => { // creates arrow function and if/then on next line
             if (!response.ok) { // if the response is ok
-                throw new Error('Network response was not ok');  // give error message
+                throw new Error('Failed to fetch product');  // give error message
             }
             return response.json(); // Parse the response as JSON
     })
 
     .then(products => { // then take products as arrow function
         products.forEach(product => {  // for each product
-            console.log(product.name); // logs the product name
+            console.log(product.fields.name); // logs the product name
         });
     })
     .catch(error => {  // if there is an error
@@ -23,3 +20,4 @@ function fetchProductsThen() {  // function to fetch the products
 }
 
 fetchProductsThen(); // calls the function
+
